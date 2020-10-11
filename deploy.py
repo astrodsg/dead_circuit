00ac77859b8e17f5663569327032494002a09b0c
earthastronaut <>
2020-10-11 11:45:50 -0600

    refactored requirements and added safety check for requirements

--- deploy.py
+++ deploy.py
@@ -14,7 +14,7 @@ from typing import Dict, List
 import urllib.request
 import zipfile
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('deploy')
 
 # copy from the project
 DEFAULT_PROJECTS_DIR = './projects'
@@ -26,8 +26,8 @@ DEFAULT_CIRCUITPY_DIR = '/Volumes/CIRCUITPY'
 REQUIREMENTS_FILENAME = 'requirements.txt'
 
 # download circuit libraries for requirements
-DEFAULT_DOWNLOAD_DIR = './untracked_downloads'
-DOWNLOAD_SOURCES = {
+DEFAULT_REQUIREMENTS_CACHE_DIR = './untracked_downloads'
+REQUIREMENTS_BUNDLES = {
     'circuitpython_busdriver': (
         "https://github.com/adafruit/Adafruit_CircuitPython_BusDevice/"
         "releases/download/"
@@ -47,6 +47,7 @@ DOWNLOAD_SOURCES = {
 
 
 def yes_no_from_user(msg):
+    """ Utility to get a yes/no answer from user and return boolean """
     msg = msg + ' [y/n]: '
     while True:
         user_input = input(msg)
@@ -73,13 +74,27 @@ def get_project_dir(projects_dir: str, project_name: str = None):
 
 
 def delete_files(location: str, user_confirm: bool = True):
+    """ Delete files at location using `rm -rf {location}`
+    """
     logger.warning(f"""
-    WARNING: About to remove files from {location} type 'n' to abort.
+
+    WARNING: Removing files from '{location}'
     """)
+    file_or_dir_to_remove = []
     for name in os.listdir(location):
         if name.startswith('.'):
             continue
         file_or_dir = os.path.join(location, name)
+        file_or_dir_to_remove.append(file_or_dir)
+
+    if len(file_or_dir_to_remove) > 50:
+        raise Exception(
+            f'Something is wrong, should not be removing '
+            f'{len(file_or_dir_to_remove)} files.\n\n'
+            f'{file_or_dir_to_remove}'
+        )
+
+    for file_or_dir in file_or_dir_to_remove:
         if not user_confirm or yes_no_from_user(f'remove {file_or_dir}?'):
             subprocess.check_call(['rm', '-rf', file_or_dir])
         else:
@@ -88,46 +103,45 @@ def delete_files(location: str, user_confirm: bool = True):
 
 
 def copy_files(source: str, destination: str = None):
-    cmd = f"cp -rp {source} {destination}"
+    """ Copy files using `cp -rp {source} {destination}` 
+    """
+    cmd = f'cp -rp {source} {destination}'
     logger.info(cmd)
-    status, msg = subprocess.getstatusoutput(cmd)
-    if status != 0:
-        raise IOError(msg)
+    subprocess.check_output(cmd, shell=True)
 
 
-def get_project_requirements(project_dir):
-    requirements = []
-    requirements_file = os.path.join(project_dir, REQUIREMENTS_FILENAME)
-    if not os.path.exists(requirements_file):
-        logger.info(f'no requirements needed: {requirements_file}')
-    else:
-        with open(requirements_file) as fp:
-            for line in fp:
-                pypackage = line.rstrip().strip()
-                if pypackage == '':
-                    continue
-                requirements.append(pypackage)
-    return requirements
-
-
-def download(url: str, downloads_dir: str):
-    output_file = os.path.join(downloads_dir, os.path.basename(url))
+def download_zipfile(url: str, destination: str):
+    """ Download a zip file and unpack it's contents into the destination """
+    output_file = os.path.join(destination, os.path.basename(url))
     urllib.request.urlretrieve(url, output_file)
     with zipfile.ZipFile(output_file) as zp:
         zp.extractall(os.path.dirname(output_file))
 
 
-def download_sources(downloads_dir: str):
-    if not os.path.isdir(downloads_dir):
-        os.mkdir(downloads_dir)
-    for name, url in DOWNLOAD_SOURCES.items():
+def download_requirements_bundles(
+        requirements_bundles: Dict,
+        requirments_cache_dir: str = DEFAULT_REQUIREMENTS_CACHE_DIR
+):
+    """ Download requirements bundles and cache 
+    
+    Requirments bundles are collections of python modules. Download each of 
+    them and then have all the modules that may be needed to select individually
+    """
+    if not os.path.isdir(requirments_cache_dir):
+        os.mkdir(requirments_cache_dir)
+    else:
+        return
+
+    for name, url in requirements_bundles.items():
         logger.debug(f'downloading {name} from {url}')
-        download(url, downloads_dir)
+        download_zipfile(url, requirments_cache_dir)
 
 
-def find_requirement(requirement: str, downloads_dir: str):
-    for dirname in os.listdir(downloads_dir):
-        dirpath = os.path.join(downloads_dir, dirname)
+def find_requirement_location(requirement: str, requirments_cache_dir: str):
+    """ Search the bundles for a required package and return the path
+    """
+    for dirname in os.listdir(requirments_cache_dir):
+        dirpath = os.path.join(requirments_cache_dir, dirname)
         if not os.path.isdir(dirpath):
             continue
 
@@ -148,33 +162,69 @@ def find_requirement(requirement: str, downloads_dir: str):
     raise FileNotFoundError(f'could not find requirement {requirement}')
 
 
-def install_requirements(
-    requirements_file: str,
-    requirements_index: Dict,
-    requirements_cache_dir: str,
-    destination_dir: str,
-):
-    # 1. get requirements from requirements file
+def read_requirements_file(requirements_file: str):
+    """ Read the requirements file and return requirements
+
+    This reads a format similar to requirements.txt and then returns the 
+    requirements.
+    """
     requirements = []
+    with open(requirements_file) as fp:
+        for line in fp:
+            package = line.rstrip().strip()
+            if package == '' or package.startswith('#'):
+                continue
+            requirements.append(package)
+    return requirements
+
+
+def download_and_copy_requirements(
+    requirements_file: str,
+    requirements_destination: str,
+    requirements_bundles: Dict = None,
+    requirments_cache_dir: str = None,
+):
+    """ Download needed requirements and copy to desination
+
+    Parameters
+        requirments_file: Custom specification similar to requirements.txt
+            that specifies individual python modules needed.
+        requirements_destination: Path to copy the python modules to.
+        requirments_bundles: Circuitpy has bundles of python libraries in the
+            form of zip releases. These bundles contain the python modules. This
+            code will download all the bundles and then search them for the
+            required python module.
+            
+            * key: arbitrary name of the bundle
+            * value: Url of the zip file for the bundle
+
+        requirments_cache_dir: Directory to download all the 
+            requirements_bundles to.
+    """
+    if requirements_bundles is None:
+        requirements_bundles = REQUIREMENTS_BUNDLES
+
+    # 1. check for requirements file
     if not os.path.exists(requirements_file):
-        logger.debug(f'no requirements needed: {requirements_file}')
+        logger.debug(f'No requirements needed. {requirements_file}')
         return
-    else:
-        with open(requirements_file) as fp:
-            for line in fp:
-                package_name = line.rstrip().strip()
-                if package_name == '' or package_name.startswith('#'):
-                    continue
-                requirements.append(package_name)
 
-    # 1. Download anc cache requirements
-    requirements_location = {}
-    for package_name in requirements:
-        url = requirements_index[package_name]
-        package_dir = download_requirement(url, requirements_cache_dir)
-        requirements_location[package_name] = package_dir
+    # 1. check for requirements destination
+    if not os.path.isdir(requirements_destination):
+        logger.debug(
+            f'Creating requirements destination {requirements_destination}')
+        os.mkdir(requirements_destination)
 
-    #
+    # 1. download all requirements bundles if not already downloaded
+    download_requirements_bundles(
+        requirements_bundles, requirments_cache_dir)
+
+    # 1. find and copy
+    requirements = read_requirements_file(requirements_file)
+    for requirement in requirements:
+        package_location = find_requirement_location(
+            requirement, requirments_cache_dir)
+        copy_files(package_location, requirements_destination)
 
 
 parser = argparse.ArgumentParser()
@@ -195,9 +245,12 @@ parser.add_argument(
     help=f'Projects directory, default={DEFAULT_PROJECTS_DIR}'
 )
 parser.add_argument(
-    '--downloads-dir',
-    default=DEFAULT_DOWNLOAD_DIR,
-    help=f'Location to download requirements, default={DEFAULT_DOWNLOAD_DIR}'
+    '--requirments-cache-dir',
+    default=DEFAULT_REQUIREMENTS_CACHE_DIR,
+    help=(
+        f'Location to download requirements bundles, '
+        f'default={DEFAULT_REQUIREMENTS_CACHE_DIR}'
+    )
 )
 
 if __name__ == "__main__":
@@ -205,7 +258,7 @@ if __name__ == "__main__":
     project_name = pargs.project
     circuitpy_dir = os.path.abspath(pargs.output)
     projects_dir = pargs.projects_dir
-    downloads_dir = pargs.downloads_dir
+    requirments_cache_dir = pargs.requirments_cache_dir
     answer_yes = pargs.yes
     user_confirm = not answer_yes
 
@@ -223,19 +276,11 @@ if __name__ == "__main__":
     # 1. copy all files from the project to the circuitpy
     copy_files(os.path.join(project_dir, '*'), circuitpy_dir)
 
-    # 1. get circuitpy requirements
-    requirements = get_project_requirements(project_dir)
-    if len(requirements) == 0:
-        sys.exit(0)
-
-    # 1. download all possible requirements locally
-    if not os.path.isdir(downloads_dir):
-        download_sources(downloads_dir)
-
-    # 1. find and copy
-    lib_destination = os.path.join(circuitpy_dir, 'lib')
-    if not os.path.isdir(lib_destination):
-        os.mkdir(lib_destination)
-    for pypackage in requirements:
-        pypackage_location = find_requirement(pypackage, downloads_dir)
-        copy_files(pypackage_location, lib_destination + '/')
+    # 1. get requirements and copy them to circuitpy
+    requirements_file = os.path.join(project_dir, 'requirements.txt')
+    requirements_destination = os.path.join(circuitpy_dir, 'lib')
+    download_and_copy_requirements(
+        requirements_file,
+        requirements_destination,
+        requirments_cache_dir=requirments_cache_dir,
+    )
